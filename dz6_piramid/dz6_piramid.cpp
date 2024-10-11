c06f6d3573dfbc37d008d89f159d4f4dcdca2889
Rom-haguko <romagureev170@gmail.com>
2024-10-11 14:52:16 +0300

    Добавьте файлы проекта.

---
+++ dz6_piramid/dz6_piramid.cpp
@@ -0,0 +1,23 @@
+﻿//нигде нет нормального объяснения работы гипотезы Эрдёша и Нормана Олера, поэтому пока только такое решение
+#include <cmatch>
+#include <iostream>
+ ; // спросить про пи
+
+
+
+using namespace std;
+void main()
+{
+
+	const double Pi = 3.141592653589793;
+	setlocale(LC_ALL, "Rus");    //Добавляем русские символы в консоль
+	int r, a;
+	cout << "Введите радиус окружности и сторону правильного треугольника: ";
+	cin >> a >> r;
+	double Sr = Pi * pow(r, 2);
+	double Sa = (pow(a, 2) * sqrt(3)) / 4;
+	cout << "Колтчество кругов: " << floor(Sa / Sr) << endl;
+
+
+
+}
